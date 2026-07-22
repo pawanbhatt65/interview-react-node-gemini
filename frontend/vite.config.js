670de2f777c32704ffa196f43f49e72547246b7a
Pawan Bhatt <2712chandra@gmail.com>
2026-07-22 08:52:43 +0530

    update vite.config.json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,6 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig(mode => {  
-  const env = loadEnv(mode, process.cwd())
-  
-  return {
-
+export default defineConfig({
   plugins: [react()],
-  base: env.VITE_BASE_PATH || '/'
-  }
-})
+});
