9a3e427385186aae32c4bdc4f99c991092ca1fbc
Pawan Bhatt <2712chandra@gmail.com>
2026-07-22 19:23:34 +0530

    update backend slash url

--- backend/src/app.js
+++ backend/src/app.js
@@ -24,9 +24,9 @@ import authRouter from "./routes/auth.route.js"
 import interviewRouter from "./routes/interview.route.js"
 
 
-app.use("/", (req, res) => {
-    res.send("Hello world.");
-});
+// app.use("/", (req, res) => {
+//     res.send("Hello world.");
+// });
 
 // auth routes
 app.use("/api/auth", authRouter);
